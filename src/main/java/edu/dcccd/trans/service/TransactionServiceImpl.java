f30d6b63701ba794b996cb38763362803b14d101
Sarah Kim <sjkim0509@gmail.com>
2019-07-25 21:55:07 -0500

    Commit changes for Lesson 11.

---
+++ src/main/java/edu/dcccd/trans/service/TransactionServiceImpl.java
@@ -0,0 +1,24 @@
+package edu.dcccd.trans.service;
+
+import edu.dcccd.trans.entity.SelectedDay;
+import edu.dcccd.trans.entity.Transaction;
+import edu.dcccd.trans.repository.SingletonTransaction;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
+public class TransactionServiceImpl implements TransactionService
+{
+    @Override
+    public void createTransaction(Transaction transaction)
+    {
+        SingletonTransaction.getInstance().transactions.add(transaction);
+    }
+    @Override
+    public List<Transaction> getAllTransaction()
+    {
+        return SingletonTransaction.getInstance().transactions;
+    }
+}
